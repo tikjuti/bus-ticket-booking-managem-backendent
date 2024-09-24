bf4e8803b541e6501e54e7a7782d658b003597a1
tikjuti <tangquoctuan2003@gmail.com>
2024-09-25 01:42:40 +0700

    inntal  backend project

---
+++ src/test/java/com/tikjuti/bus_ticket_booking/BusTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tikjuti.bus_ticket_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
